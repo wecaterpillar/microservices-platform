e19bbc001d299c8ae0f7be3cf49c811fe6ef812e
dev <dev@dev.com>
2022-06-26 23:22:34 +0800

    update

--- package.bat
+++ package.bat
@@ -1,18 +1,18 @@
 @echo on
 @echo =============================================================
 @echo $                                                           $
-@echo $               OpenEA ESP                  $
+@echo $               OpenEA MSP                                  $
 @echo $                                                           $
 @echo $                                                           $
 @echo $                                                           $
-@echo $  OpenEA All Right Reserved                                   $
+@echo $  OpenEA All Right Reserved                                $
 @echo $  Copyright (C) 2019-2050                                  $
 @echo $                                                           $
 @echo =============================================================
 @echo.
 @echo off
 
-@title OpenEA ESP
+@title OpenEA MSP
 @color 0e
 
 call mvn clean package -Dmaven.test.skip=true
